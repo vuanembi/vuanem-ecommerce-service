cc2de1414d269230abd99200fa8decd4f1df9d9c
HM <hieumdd@gmail.com>
2021-12-02 15:40:41 +0700

    feat: Added print

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main(request) -> dict:
     request_json: dict = request.get_json()
     request_path: str = request.path
 
+    print(request_path, request_json)
+
     if request_path == "/tiki":
         response = handle_event_queue()
 
