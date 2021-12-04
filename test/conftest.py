90df5797819e1a8a7a08724853866c746823747a
HM <hieumdd@gmail.com>
2021-12-04 13:52:47 +0700

    refactor: Refactoed test suites

---
+++ test/conftest.py
@@ -0,0 +1,13 @@
+import requests
+import pytest
+
+from libs.restlet import netsuite_session
+
+@pytest.fixture()
+def session():
+    return requests.Session()
+
+
+@pytest.fixture()
+def oauth_session():
+    return netsuite_session()
--- test/test_telegram.py
+++ test/test_telegram.py
@@ -8,7 +8,12 @@ def order():
     return "11111"
 
 
-@pytest.mark.parametrize("ecommerce", ["Tiki"])
+@pytest.mark.parametrize(
+    "ecommerce",
+    [
+        "Tiki",
+    ],
+)
 def test_send_created_order(ecommerce, order):
     res = send_created_order(ecommerce, order)
     assert res["ok"]
