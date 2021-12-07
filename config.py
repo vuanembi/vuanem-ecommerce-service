e07eb893c0645980ff0fde3de94c49111857593f
HM <hieumdd@gmail.com>
2021-12-07 17:48:19 +0700

    feat: Added TIKI dev queue

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ ENV = {
     "dev": {
         "PUBLIC_URL": "https://us-central1-voltaic-country-280607.cloudfunctions.net/vuanem-ecommerce-service-dev",
         "TELEGRAM_CHAT_ID": "-645664226",
-        "TIKI_QUEUE_CODE": "6cd68367-3bde-4aac-a24e-258bc907d68b",
+        "TIKI_QUEUE_CODE": "f0c586e1-fb27-4d73-90bb-bcfe31464dba",
     },
     "prod": {
         "PUBLIC_URL": "https://us-central1-voltaic-country-280607.cloudfunctions.net/vuanem-ecommerce-service",
