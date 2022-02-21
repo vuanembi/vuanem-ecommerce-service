2a6a687097e7f9b6d72655535a1ab51effeab9ba
HM <hieumdd@gmail.com>
2022-02-21 16:18:53 +0700

    feat: Added journal_entry_controller to netsuite_controller

--- netsuite/netsuite_controller.py
+++ netsuite/netsuite_controller.py
@@ -4,6 +4,7 @@ from flask import Request, abort
 from returns.result import Success
 
 from netsuite.query import query_controller
+from netsuite.journal_entry import journal_entry_controller
 from netsuite.order import order_service
 
 
@@ -15,31 +16,30 @@ query_path = [
 
 def netsuite_controller(request: Request) -> dict[str, Any]:
     body: Optional[dict[str, Any]] = request.get_json()
-    if body:
-        for path in query_path:
-            if path in request.path:
-                return query_controller.query_controller(request)
-        if request.path == "/netsuite/order":
-            if request.method in ["POST", "PUT"] and "prepared_id" in body:
-                svc = (
-                    order_service.close
-                    if request.method == "PUT"
-                    else order_service.create
+    for path in query_path:
+        if path in request.path:
+            return query_controller.query_controller(request)
+    if "journal_entry" in request.path :
+        return journal_entry_controller.journal_entry_controller(request)
+    if request.path == "/netsuite/order" and body:
+        if request.method in ["POST", "PUT"] and "prepared_id" in body:
+            svc = (
+                order_service.close
+                if request.method == "PUT"
+                else order_service.create
+            )
+            return (
+                svc(body["prepared_id"])  # type: ignore
+                .map(
+                    lambda x: {
+                        "status": 200,
+                        "result": x[0],
+                    }
                 )
-                return (
-                    svc(body["prepared_id"])  # type: ignore
-                    .map(
-                        lambda x: {
-                            "status": 200,
-                            "result": x[0],
-                        }
-                    )
-                    .lash(lambda x: Success(abort(400, repr(x[0]))))
-                ).unwrap()
+                .lash(lambda x: Success(abort(400, repr(x[0]))))
+            ).unwrap()
 
-            else:
-                return abort(400)
         else:
-            return abort(404)
+            return abort(400)
     else:
-        return abort(400)
+        return abort(404)
--- test/test_netsuite.py
+++ test/test_netsuite.py
@@ -110,8 +110,12 @@ class TestAnalytics:
 
 
 class TestBankInTransit:
-    def test_bank_in_transit_service(self):
+    def test_service(self):
         res = journal_entry_service.bank_in_transit_service(
             date.today() - timedelta(days=1)
         )
+        res
+
+    def test_controller(self):
+        res = run("/netsuite/journal_entry/bank_in_transit")
         res
