b82e7d3ad9596a63ad2cf2339007d6543e45c5c9
HM <hieumdd@gmail.com>
2022-02-21 15:30:18 +0700

    feat: Added send_email to bank_in_transit_service

--- test/test_netsuite.py
+++ test/test_netsuite.py
@@ -1,3 +1,5 @@
+from datetime import date, timedelta
+
 from returns.pipeline import is_successful
 
 from netsuite.query.analytics import analytics_service
@@ -109,5 +111,7 @@ class TestAnalytics:
 
 class TestBankInTransit:
     def test_bank_in_transit_service(self):
-        res = journal_entry_service.bank_in_transit_service()
+        res = journal_entry_service.bank_in_transit_service(
+            date.today() - timedelta(days=1)
+        )
         res
