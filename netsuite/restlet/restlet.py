7514f7169c87fd31721f5bd7898e134d47e9a132
HM <hieumdd@gmail.com>
2022-05-23 10:39:27 +0700

    feat: Add Voucher Adjustments Service

--- netsuite/restlet/restlet.py
+++ netsuite/restlet/restlet.py
@@ -6,8 +6,6 @@ class Restlet(TypedDict):
     deploy: int
 
 
-# SalesOrder: Restlet = {"script": 997, "deploy": 1}
-# Customer: Restlet = {"script": 1099, "deploy": 1}
 Record: Restlet = {"script": 1236, "deploy": 1}
 
 SavedSearch: Restlet = {"script": 1208, "deploy": 1}
@@ -15,3 +13,4 @@ Analytics: Restlet = {"script": 1098, "deploy": 1}
 
 CSVImportTask: Restlet = {"script": 1233, "deploy": 1}
 BankInTransitTask: Restlet = {"script": 1234, "deploy": 1}
+VoucherAdjustmentsTask: Restlet = {"script": 1271, "deploy": 1}
